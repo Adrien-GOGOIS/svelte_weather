dd6c8d58b707e9539f3fc11674c17c08adbc4c83
Adrien GOGOIS <ad.gogois@gmail.com>
2023-07-20 13:43:02 +0200

    Improve style of airQualityCard

--- src/components/CurrentAirQualityCard.svelte
+++ src/components/CurrentAirQualityCard.svelte
@@ -1,13 +1,31 @@
 <script lang="ts">
 	import { AirQualityIndex, type AirQuality } from "../types/AirQuality";
-	import type { CurrentWeatherResponse } from "../types/Cities";
-	import { getRequestTime } from "../utils/dateService";
 
-	export let cityCurrentWeather: CurrentWeatherResponse;
 	export let airQuality: AirQuality;
+	let airQualityIndexDescription: string;
+	let airQualityBackground: string;
 
-	const getAirQualityIndex = (airQuality: AirQuality): string => {
-		return Object.values(AirQualityIndex)[airQuality.us_epa_index];
+	const getAirQualityIndex = (airQualityIndex: number): string => {
+		airQualityIndexDescription = Object.values(AirQualityIndex)[airQualityIndex];
+		if (airQualityIndex === 1) {
+			return 'bg-good'
+		}
+		if (airQualityIndex === 2) {
+			return 'bg-moderate'
+		}
+		if (airQualityIndex === 3) {
+			return 'bg-unhealthy-for-some'
+		}
+		if (airQualityIndex === 4) {
+			return 'bg-unhealthy'
+		}
+		if (airQualityIndex === 5) {
+			return 'bg-very-unhealthy'
+		}
+		if (airQualityIndex === 6) {
+			return 'bg-hazardous'
+		}
+		return ''
 	}
 </script>
 
@@ -16,24 +34,14 @@
 		<div class="container py-5 h-100">
 			<div class="row d-flex justify-content-center align-items-center h-100">
 				<div class="col-md-8 col-lg-6 col-xl-4">
-					<div class={`card text-light ${getAirQualityIndex(airQuality)}`}>
+					<div class={`card ${getAirQualityIndex(airQuality.us_epa_index)}`}>
 						<div class="card-body p-4">
 							<div class="d-flex">
-								<h6 class="flex-grow-1">{cityCurrentWeather.location.name}, {cityCurrentWeather.location.country}</h6>
-								<h6>{getRequestTime(cityCurrentWeather.location.localtime_epoch)}</h6>
+								<h6 class="flex-grow-1">Indice de qualité de l'air</h6>
 							</div>
-				
 							<div class="d-flex flex-column text-center mt-4 mb-4">
 								<h6 class="display-4 mb-0 font-weight-bold">{airQuality.us_epa_index}</h6>
-								<span class="small">Indice de pollution</span>
-							</div>
-							<div class="d-flex align-items-center small mt-4">
-								<div class="flex-grow-1">
-									<div><span class="ms-1">Ozone (O₃): {airQuality.o3.toFixed(2)}µg/m³</span></div>
-									<div><span class="ms-1">Dioxyde d'Azote (NO₂): {airQuality.no2.toFixed(2)}µg/m³</span></div>
-									<div><span class="ms-1">Particules (PM₁₀): {airQuality.pm10.toFixed(2)}µg/m³</span></div>
-									<div><span class="ms-1">Particules fines (PM₂.₅): {airQuality.pm2_5.toFixed(2)}µg/m³</span></div>
-								</div>
+								<span class="small">{airQualityIndexDescription}</span>
 							</div>
 						</div>
 					</div>
@@ -42,3 +50,26 @@
 		</div>
 	</section>
 {/if}
+
+<style>
+	.bg-good {
+		background-color: rgba(129, 237, 229, 0.8);
+	}
+	.bg-moderate {
+		background-color: rgba(122, 202, 163, 0.8);
+	}
+	.bg-unhealthy-for-some {
+		background-color: rgba(238, 229, 97, 0.8);
+	}
+	.bg-unhealthy {
+		background-color: rgba(233, 131, 82, 0.8);
+	}
+	.bg-very-unhealthy {
+		background-color: rgba(225, 6, 43, 0.8);
+		color: white;
+	}
+	.bg-hazardous {
+		background-color: rgba(39, 13, 43, 0.8);
+		color: white;
+	}
+</style>
